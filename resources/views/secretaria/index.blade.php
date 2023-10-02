494e9faabb00371b80044d94bf2f71f7888bab3a
lvlMiria <m.pilar.bruna@gmail.com>
2023-10-01 21:12:28 -0300

    pantalla secretaria

---
+++ resources/views/secretaria/index.blade.php
@@ -0,0 +1,41 @@
+@extends('layouts.master')
+@section('title','Inicio Secretaria - Centro Médico Epojé')
+@section('contenido')
+    {{-- <div class="row align-items-center">
+        <div class = 'col-6 mt-5'>
+            <img src="{{asset('images/logo.jpeg')}}" alt="Logo Centro" class="img-fluid">
+        </div>
+    </div> --}}
+
+    <div class="container-fluid mt-4 d-flex justify-content-center" style="background-color: #88bd9e">
+        <div class="col-5 d-flex justify-content-center">
+            <div class="row m-4">
+                <div class="card border-light mb-3" style="background-color: #88bd9e">
+                    <div class="card-body">
+                        <div class="row">
+                            <h4 class = "text-center" style="color: white">Bienvenida {{auth()->user()->nom_usuario}} {{auth()->user()->apep_usuario}}</h4>
+                        </div>
+                        <div class="row m-4">
+                            <div class="col d-flex justify-content-center">
+                                <img src="{{asset('images/icono_usuario.png')}}" alt="icono usuario" class="img-fluid" style="width: 10rem">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-7 d-flex flex-column justify-content-center">
+            <div class="row m-4">
+                <a class=" btn btn-light btn-lg" href="">Agendar Hora</a>
+            </div>
+            <div class="row m-4">
+                <a class=" btn btn-outline-light btn-lg" href="{{ route('pacientes.index') }}">Administar Pacientes</a>
+            </div>
+            <div class="row m-4">
+                <a class=" btn btn-outline-light btn-lg" href="">Administar Horarios</a>
+            </div>
+        </div>
+
+    </div>
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\RegistroController;
 use App\Http\Controllers\SessionController;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\PacientesController;
+use App\Http\Controllers\SecretariaController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -49,3 +50,5 @@ Route::put('/pacientes/{paciente}',[PacientesController::class,'update'])->name(
 Route::get('/admin/usuario',[AdminController::class,'showUsuario'])->name('admin.showUsuario');
 Route::get('/admin/profesional',[AdminController::class,'showProfesional'])->name('admin.showProfesional');
 Route::get('/admin/especialidad',[AdminController::class,'showEspecialidad'])->name('admin.showEspecialidad');
+
+Route::get('/secretaria',[SecretariaController::class,'index'])->name('secretaria.index');
