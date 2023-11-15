0f2081acf6bf816cf6a5d4b2212aa00da4353a87
lvlMiria <m.pilar.bruna@gmail.com>
2023-11-15 20:08:29 -0300

    tabla, modelo y contrller de horarios

---
+++ database/migrations/2023_11_15_225230_create_disponibilidad_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('disponibilidad', function (Blueprint $table) {
+            $table->string('rut_profesional',11);
+            $table->tinyInteger('dia')->unsigned();
+            $table->string('hora_inicio');
+            $table->string('hora_fin');
+            
+            $table->foreign('rut_profesional')->references('rut_profesional')->on('profesionales');
+            $table->primary(['rut_profesional','hora_inicio','hora_fin']);
+
+            //$table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('disponibilidad');
+    }
+};
